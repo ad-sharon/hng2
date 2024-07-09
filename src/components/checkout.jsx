3b7b53682d2e053807585291074f8767da4c3d9d
Sharon <omuwaste@gmail.com>
2024-07-09 16:50:29 +0100

    finished checkout

--- src/components/checkout.jsx
+++ src/components/checkout.jsx
@@ -5,6 +5,7 @@ import CartItem2 from "./cart_item2";
 import CheckOutButton from "./checkout_b";
 import Checkout_dropdown from "./checkout_dropdown";
 import expand from "../assets/images/expand_down.png";
+import CheckoutForm from "./checkout_form";
 
 const Checkout = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -14,307 +15,310 @@ const Checkout = () => {
   };
 
   return (
-    <div
-      style={{
-        width: 1200,
-        height: 577,
-        top: 204,
-        left: 120,
-        display: "flex",
-        flexDirection: "column",
-        position: "absolute",
-      }}
-    >
+    <div>
       <div
         style={{
           width: 1200,
-          height: 56,
-          justifyContent: "space-between",
+          height: 577,
+          top: 204,
+          left: 120,
           display: "flex",
-          flexDirection: "row",
-          backgroundColor: "#EEE4E3",
-          alignItems: "center",
-          padding: "12px 16px",
+          flexDirection: "column",
+          position: "absolute",
         }}
       >
         <div
           style={{
-            fontFamily: "Kanit",
-            fontWeight: "400",
-            fontSize: "24px",
-            lineHeight: "26px",
-            letterSpacing: "0.12px",
-            textAlign: "center",
+            width: 1200,
+            height: 56,
+            justifyContent: "space-between",
+            display: "flex",
+            flexDirection: "row",
+            backgroundColor: "#EEE4E3",
+            alignItems: "center",
+            padding: "12px 16px",
           }}
         >
-          Order Summary
+          <div
+            style={{
+              fontFamily: "Kanit",
+              fontWeight: "400",
+              fontSize: "24px",
+              lineHeight: "26px",
+              letterSpacing: "0.12px",
+              textAlign: "center",
+            }}
+          >
+            Order Summary
+          </div>
+
+          <div
+            style={{
+              fontFamily: "Kanit",
+              fontWeight: "400",
+              fontSize: "24px",
+              lineHeight: "26px",
+              letterSpacing: "0.12px",
+              textAlign: "center",
+              color: "#473838",
+            }}
+          >
+            Total
+          </div>
         </div>
 
-        <div
-          style={{
-            fontFamily: "Kanit",
-            fontWeight: "400",
-            fontSize: "24px",
-            lineHeight: "26px",
-            letterSpacing: "0.12px",
-            textAlign: "center",
-            color: "#473838",
-          }}
-        >
-          Total
-        </div>
-      </div>
-
-      <div
-        style={{
-          width: 1200,
-          height: 465,
-          gap: 84,
-          display: "flex",
-          flexDirection: "row",
-        }}
-      >
-        {/* 2 cart items */}
         <div
           style={{
             width: 1200,
             height: 465,
-            gap: 41,
+            gap: 84,
             display: "flex",
-            flexDirection: "column",
+            flexDirection: "row",
           }}
         >
+          {/* 2 cart items */}
           <div
             style={{
-              width: 767,
-              height: 465,
-            }}
-          >
-            <CartItem1 />
-          </div>
-          <div
-            style={{
-              width: 767,
+              width: 1200,
               height: 465,
               gap: 41,
               display: "flex",
               flexDirection: "column",
             }}
           >
-            <CartItem2 />
+            <div
+              style={{
+                width: 767,
+                height: 465,
+              }}
+            >
+              <CartItem1 />
+            </div>
+            <div
+              style={{
+                width: 767,
+                height: 465,
+                gap: 41,
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <CartItem2 />
+            </div>
           </div>
-        </div>
 
-        <div
-          style={{
-            width: 359,
-            height: 425,
-            display: "flex",
-            flexDirection: "column",
-          }}
-        >
           <div
             style={{
               width: 359,
-              height: 194,
-              gap: 29,
+              height: 425,
               display: "flex",
               flexDirection: "column",
             }}
           >
             <div
               style={{
-                width: 349,
-                height: 50,
+                width: 359,
+                height: 194,
+                gap: 29,
                 display: "flex",
-                flexDirection: "row",
+                flexDirection: "column",
               }}
             >
-              {/* 1 */}
               <div
                 style={{
-                  width: 248,
+                  width: 349,
                   height: 50,
-                  padding: "12px 16px",
-                  border: "1px solid #D9D9D9",
+                  display: "flex",
+                  flexDirection: "row",
                 }}
               >
-                <p
+                {/* 1 */}
+                <div
                   style={{
-                    width: 216,
-                    color: "#B2A6A6",
-                    height: 26,
-                    letterSpacing: 0.09,
-                    size: 18,
-                    fontWeight: 400,
-                    fontFamily: "Kanit",
+                    width: 248,
+                    height: 50,
+                    padding: "12px 16px",
+                    border: "1px solid #D9D9D9",
                   }}
                 >
-                  Discount Code
-                </p>
+                  <p
+                    style={{
+                      width: 216,
+                      color: "#B2A6A6",
+                      height: 26,
+                      letterSpacing: 0.09,
+                      size: 18,
+                      fontWeight: 400,
+                      fontFamily: "Kanit",
+                    }}
+                  >
+                    Discount Code
+                  </p>
+                </div>
+
+                {/* 2 */}
+                <div
+                  style={{
+                    width: 111,
+                    height: 50,
+                    backgroundColor: "#ED8174",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <p
+                    style={{
+                      color: "#fff",
+                      height: 26,
+                      letterSpacing: 0.09,
+                      size: 18,
+                      fontWeight: 400,
+                      fontFamily: "Kanit",
+                    }}
+                  >
+                    Apply
+                  </p>
+                </div>
               </div>
 
-              {/* 2 */}
               <div
                 style={{
-                  width: 111,
-                  height: 50,
-                  backgroundColor: "#ED8174",
+                  width: 359,
+                  height: 32,
                   display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
+                  justifyContent: "space-between",
                 }}
               >
                 <p
                   style={{
-                    color: "#fff",
+                    width: 329,
                     height: 26,
                     letterSpacing: 0.09,
                     size: 18,
                     fontWeight: 400,
                     fontFamily: "Kanit",
+                    color: "#473838",
                   }}
                 >
-                  Apply
+                  Shipping Fee
+                </p>
+
+                <p
+                  style={{
+                    width: 38,
+                    height: 32,
+                    letterSpacing: 0.09,
+                    size: 24,
+                    textAlign: "center",
+                    fontWeight: 700,
+                    fontFamily: "Kanit",
+                  }}
+                >
+                  $0
                 </p>
               </div>
             </div>
 
             <div
               style={{
-                width: 359,
-                height: 32,
                 display: "flex",
-                justifyContent: "space-between",
+                border: "1px solid #D9D9D9",
+                justifyContent: "center",
+                alignItems: "center",
               }}
             >
-              <p
+              <Checkout_dropdown />
+
+              <img
+                src={expand}
+                style={{ width: 24, height: 24 }}
+                onClick={toggle_dropdown}
+              />
+              {isOpen ? "" : ""}
+            </div>
+
+            {isOpen && (
+              <div
                 style={{
-                  width: 329,
-                  height: 26,
+                  width: 359,
+                  height: 54,
+                  alignItems: "center",
                   letterSpacing: 0.09,
                   size: 18,
                   fontWeight: 400,
                   fontFamily: "Kanit",
-                  color: "#473838",
+                  color: "#B2A6A6",
+                  backgroundColor: "#fff",
                 }}
               >
-                Shipping Fee
-              </p>
+                <option>Door Delivery</option>
+                <option>Pick Up</option>
+              </div>
+            )}
 
-              <p
-                style={{
-                  width: 38,
-                  height: 32,
-                  letterSpacing: 0.09,
-                  size: 24,
-                  textAlign: "center",
-                  fontWeight: 700,
-                  fontFamily: "Kanit",
-                }}
-              >
-                $0
-              </p>
-            </div>
-          </div>
-
-          <div
-            style={{
-              display: "flex",
-              border: "1px solid #D9D9D9",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <Checkout_dropdown />
-
-            <img
-              src={expand}
-              style={{ width: 24, height: 24 }}
-              onClick={toggle_dropdown}
-            />
-            {isOpen ? "" : ""}
-          </div>
-
-          {isOpen && (
             <div
               style={{
                 width: 359,
-                height: 54,
-                alignItems: "center",
-                letterSpacing: 0.09,
-                size: 18,
-                fontWeight: 400,
-                fontFamily: "Kanit",
-                color: "#B2A6A6",
-                backgroundColor: "#fff",
-              }}
-            >
-              <option>Door Delivery</option>
-              <option>Pick Up</option>
-            </div>
-          )}
-
-          <div
-            style={{
-              width: 359,
-              height: 231,
-              justifyContent: "space-between",
-              alignItems: "flex-end",
-              display: "flex",
-            }}
-          >
-            <div
-              style={{
-                width: 359,
-                height: 55,
-                padding: "12px 16px",
-                border: "1px solid #D9D9D9",
+                height: 231,
                 justifyContent: "space-between",
-
+                alignItems: "flex-end",
                 display: "flex",
               }}
             >
               <div
                 style={{
-                  width: 70,
-                  height: 26,
-                  letterSpacing: 0.09,
+                  width: 359,
+                  height: 55,
+                  padding: "12px 16px",
+                  border: "1px solid #D9D9D9",
+                  justifyContent: "space-between",
 
-                  fontFamily: "Kanit",
-                  fontWeight: 400,
-                  size: "18px",
-                  color: "#473838",
-                  whiteSpace: "nowrap",
+                  display: "flex",
                 }}
               >
-                Subtotal
-              </div>
+                <div
+                  style={{
+                    width: 70,
+                    height: 26,
+                    letterSpacing: 0.09,
 
-              <div
-                style={{
-                  width: 52,
-                  height: 32,
-                  color: "#473838",
-                  fontSize: 24,
-                  fontFamily: "Kanit",
-                  fontWeight: "700",
-                  letterSpacing: 0.12,
-                }}
-              >
-                $186
+                    fontFamily: "Kanit",
+                    fontWeight: 400,
+                    size: "18px",
+                    color: "#473838",
+                    whiteSpace: "nowrap",
+                  }}
+                >
+                  Subtotal
+                </div>
+
+                <div
+                  style={{
+                    width: 52,
+                    height: 32,
+                    color: "#473838",
+                    fontSize: 24,
+                    fontFamily: "Kanit",
+                    fontWeight: "700",
+                    letterSpacing: 0.12,
+                  }}
+                >
+                  $186
+                </div>
               </div>
             </div>
           </div>
         </div>
+        <div
+          style={{
+            width: 1200,
+            height: 56,
+            backgroundColor: "#EEE4E3",
+          }}
+        ></div>
       </div>
-      <div
-        style={{
-          width: 1200,
-          height: 56,
-          backgroundColor: "#EEE4E3",
-        }}
-      ></div>
+      <CheckoutForm />
     </div>
   );
 };
---
+++ src/components/checkout_form.jsx
@@ -0,0 +1,254 @@
+import React from "react";
+import "../hover_product.css";
+import visa from "../assets/images/visa_card.png";
+const CheckoutForm = () => {
+  return (
+    <div
+      style={{
+        width: 548,
+        height: 414,
+        top: 857,
+        left: 446,
+        gap: 16,
+        display: "flex",
+        flexDirection: "column",
+        margin: "auto",
+        position: "absolute",
+      }}
+    >
+      <div
+        style={{
+          backgroundColor: "#EEE4E3",
+          height: 56,
+          padding: "12px 16px",
+          letterSpacing: 0.09,
+          fontFamily: "Kanit",
+          fontWeight: 400,
+          size: "24px",
+          color: "#473838",
+          whiteSpace: "nowrap",
+        }}
+      >
+        Delivery
+      </div>
+
+      <div
+        style={{
+          width: 548,
+          height: 342,
+          gap: 22,
+          display: "flex",
+          flexDirection: "column",
+          fontFamily: "Kanit",
+          size: 18,
+          fontWeight: 400,
+          letterspacing: 0.005,
+          color: "#B2A6A6",
+        }}
+      >
+        <input
+          style={{
+            padding: "12px 16px",
+            width: "100%",
+            height: 50,
+            border: "1px solid #d9d9d9",
+          }}
+          placeholder="First Name"
+        ></input>
+        <input
+          style={{
+            padding: "12px 16px",
+            width: "100%",
+            height: 50,
+            border: "1px solid #d9d9d9",
+          }}
+          placeholder="Last Name"
+        ></input>
+        <input
+          style={{
+            padding: "12px 16px",
+            width: "100%",
+            height: 50,
+            border: "1px solid #d9d9d9",
+          }}
+          placeholder="Email"
+        ></input>
+        <input
+          style={{
+            width: "100%",
+            padding: "12px 16px",
+            height: 50,
+            border: "1px solid #d9d9d9",
+          }}
+          placeholder="Phone"
+        ></input>
+        <input
+          style={{
+            width: "100%",
+            padding: "12px 16px",
+            height: 50,
+            border: "1px solid #d9d9d9",
+          }}
+          placeholder="Address"
+        ></input>
+      </div>
+
+      <div
+        style={{
+          width: 554,
+          height: 386,
+          top: 1319,
+          left: 446,
+          gap: 24,
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <div
+          style={{
+            width: 554,
+            height: 90,
+            gap: 8,
+            letterSpacing: 0.09,
+            fontFamily: "Kanit",
+            fontWeight: 400,
+            color: "#473838",
+            display: "flex",
+            flexDirection: "column",
+            whiteSpace: "nowrap",
+          }}
+        >
+          <div
+            style={{
+              backgroundColor: "#EEE4E3",
+              width: 554,
+              height: 56,
+              padding: "12px 16px",
+              letterSpacing: 0.09,
+              size: "24px",
+            }}
+          >
+            Payment
+          </div>
+
+          <div
+            style={{
+              width: 554,
+              height: 26,
+              size: "18px",
+            }}
+          >
+            Choose your payment method
+          </div>
+        </div>
+
+        <div
+          style={{
+            width: 554,
+            height: 178,
+            gap: 8,
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <label
+            style={{
+              alignContent: "center",
+              width: "100%",
+              height: 54,
+              border: "6px solid #eee4e3",
+            }}
+          >
+            <input type="radio" />
+            <div
+              style={{
+                width: "97%",
+                display: "flex",
+                position: "relative",
+                top: -30,
+                left: 25,
+                flexDirection: "row",
+                justifyContent: "space-between",
+              }}
+            >
+              Credit Card
+              <img style={{ width: 137.4, height: 30 }} src={visa} />
+            </div>
+          </label>
+
+          <input
+            style={{
+              padding: "12px 16px",
+              width: "100%",
+              height: 50,
+              border: "1px solid #d9d9d9",
+            }}
+            placeholder="Card Number"
+          ></input>
+
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <input
+              style={{
+                width: 264,
+                padding: "12px 16px",
+                height: 50,
+                border: "1px solid #d9d9d9",
+              }}
+              placeholder="CVV"
+            ></input>
+            <input
+              style={{
+                width: 264,
+                padding: "12px 16px",
+                height: 50,
+                border: "1px solid #d9d9d9",
+              }}
+              placeholder="Expiry Date"
+            ></input>
+          </div>
+        </div>
+
+        <label
+          style={{
+            width: "554",
+            height: 54,
+            border: "6px solid #eee4e3",
+          }}
+        >
+          <input type="radio" />
+          Transfer
+        </label>
+      </div>
+
+      <div
+        style={{
+          width: 554,
+          height: 50,
+          top: 1790,
+          left: 446,
+          gap: 8,
+          backgroundColor: "#ED8174",
+          padding: "12px 16px",
+        }}
+      >
+        <p
+          style={{
+            width: 292,
+            height: 26,
+            letterSpacing: 0.09,
+            textAlign: "center",
+            fontFamily: "Kanit",
+            fontWeight: 400,
+            margin: "auto",
+            size: "18px",
+            color: "#fff",
+          }}
+        >
+          Pay Now
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default CheckoutForm;
