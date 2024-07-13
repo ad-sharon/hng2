8bd69dff0fd3f940a332e697bc597fc0031244ad
Sharon <omuwaste@gmail.com>
2024-07-13 15:44:46 +0100

    add cart, remove from cart done

---
+++ src/components/cart_context.js
@@ -0,0 +1,31 @@
+import React, { createContext, useContext, useState } from "react";
+
+export const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState();
+
+  const addToCart = (product) => {
+    setCart((prevCart) => [...prevCart, product]);
+  };
+
+  const removeFromCart = (productId) => {
+    setCart((prevCart) =>
+      prevCart.filter((product) => product.unique_id !== productId)
+    );
+  };
+
+  const clearCart = () => {
+    setCart([]);
+  };
+
+  return (
+    <CartContext.Provider
+      value={{ cart, addToCart, removeFromCart, clearCart }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export const useCart = () => useContext(CartContext);
