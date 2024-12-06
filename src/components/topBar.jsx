c81e167a297121b690bbef55833100dcab54d2fb
Sharon <omuwaste@gmail.com>
2024-12-06 16:19:29 +0100

    fix: edit topbar.jsx to use tailwind

--- src/components/topBar.jsx
+++ src/components/topBar.jsx
@@ -1,45 +1,31 @@
 import React from "react";
-import { Box, useBreakpointValue } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
 
 const TopBar = () => {
-  const displayText = useBreakpointValue({
-    base: "FREE SHIPPING ON EVERY PRODUCT FROM THE 22ND...",
-    sm: "FREE SHIPPING ON EVERY PRODUCT FROM THE 22ND OF JULY",
-  });
+  const [isSmallScreen, setisSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setisSmallScreen(window.innerWidth >= 640); //gives true if width is bigger than 640px
+    };
+
+    handleResize();
+
+    // resize listener for responsiveness changes
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return (
-    <Box
-      style={{
-        backgroundColor: "#EEE4E3",
-        height: "52px",
-        width: "100%",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <Box
-        style={{
-          height: "26px",
-          top: "14px",
-          left: "470px",
-        }}
-      >
-        <h3
-          style={{
-            fontWeight: "400",
-            fontSize: "90%",
-
-            color: "#241C1C",
-            fontFamily: "Kanit",
-            lineHeight: "26px",
-            whiteSpace: "nowrap",
-          }}
-        >
-          {displayText}
-        </h3>
-      </Box>
-    </Box>
+    <div className="bg-[#EEE4E3] h-[52px] w-full flex justify-center items-center">
+      <h3 className="font-normal font-kanit color-[241C1C] text-[90%]/[26px] whitespace-nowrap">
+        {isSmallScreen
+          ? "FREE SHIPPING ON EVERY PRODUCT FROM THE 22ND OF JULY"
+          : "FREE SHIPPING ON EVERY PRODUCT FROM THE 22ND..."}
+      </h3>
+    </div>
   );
 };
 
